feb6f17ea5018579c35a10b9b1ad2d3ae6a52761
Coral1786 <u21211786@utp.edu.pe>
2025-10-29 23:10:34 -0500

    Add SQL query for product and subcategory names

---
+++ Semana12/DemoSubqueryString.sql
@@ -0,0 +1,11 @@
+SELECT 
+    p.EnglishProductName,
+    ps.EnglishProductSubcategoryName
+FROM DimProduct AS p
+INNER JOIN DimProductSubcategory AS ps
+    ON p.ProductSubcategoryKey = ps.ProductSubcategoryKey
+WHERE p.EnglishProductName LIKE CONCAT('%', (
+    SELECT TOP 1 ps2.EnglishProductSubcategoryName
+    FROM DimProductSubcategory AS ps2
+    WHERE ps2.ProductSubcategoryKey = p.ProductSubcategoryKey
+), '%');
