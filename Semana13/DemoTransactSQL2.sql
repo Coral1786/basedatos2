973c7b1c04c421d7dbaa2372f39b15d9cc356b28
Coral1786 <u21211786@utp.edu.pe>
2025-11-07 23:39:08 -0500

    Create DemoTransactSQL2.sql

---
+++ Semana13/DemoTransactSQL2.sql
@@ -0,0 +1,32 @@
+USE AdventureWorksDW2022;
+GO
+
+-- Remove existing version if present
+IF OBJECT_ID('dbo.fn_GetTopResellersByYear', 'IF') IS NOT NULL
+    DROP FUNCTION dbo.fn_GetTopResellersByYear;
+GO
+
+CREATE FUNCTION dbo.fn_GetTopResellersByYear
+(
+    @Year INT
+)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT 
+        t.SalesTerritoryRegion,
+        r.ResellerName,
+        SUM(f.SalesAmount) AS TotalSales,
+        COUNT(f.SalesOrderNumber) AS OrdersCount
+    FROM FactResellerSales AS f
+    INNER JOIN DimReseller AS r
+        ON f.ResellerKey = r.ResellerKey
+    INNER JOIN DimSalesTerritory AS t
+        ON f.SalesTerritoryKey = t.SalesTerritoryKey
+    INNER JOIN DimDate AS d
+        ON f.OrderDateKey = d.DateKey
+    WHERE d.CalendarYear = @Year
+    GROUP BY t.SalesTerritoryRegion, r.ResellerName
+);
+GO
