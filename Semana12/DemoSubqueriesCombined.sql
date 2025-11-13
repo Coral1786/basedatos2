bf485d9c0ad7a49b731afe1ba7bc1f0278fd41aa
Coral1786 <u21211786@utp.edu.pe>
2025-11-13 11:01:10 -0500

    Update DemoSubqueriesCombined.sql

--- Semana12/DemoSubqueriesCombined.sql
+++ Semana12/DemoSubqueriesCombined.sql
@@ -1,14 +1,22 @@
 USE AdventureWorksDW2022;
 GO
 
-SELECT 
-    (SELECT 'Q' + CAST(DATEPART(QUARTER, CAST(d.FullDateAlternateKey AS DATETIME)) AS VARCHAR(1))
-            + ' - ' + CAST(d.CalendarYear AS VARCHAR(4))
-    ) AS QuarterLabel,
+SELECT TOP 5
+    r.ResellerName,
+    t.SalesTerritoryRegion,
+    
+    DATENAME(MONTH, MIN(d.FullDateAlternateKey)) + ' ' + CAST(MIN(d.CalendarYear) AS VARCHAR(4)) AS FormattedOrderMonth,
     SUM(f.SalesAmount) AS TotalSales
-FROM FactInternetSales AS f
+FROM FactResellerSales AS f
+INNER JOIN DimReseller AS r
+    ON f.ResellerKey = r.ResellerKey
+INNER JOIN DimSalesTerritory AS t
+    ON f.SalesTerritoryKey = t.SalesTerritoryKey
 INNER JOIN DimDate AS d
     ON f.OrderDateKey = d.DateKey
-WHERE d.CalendarYear BETWEEN 2012 AND 2013
-GROUP BY DATEPART(QUARTER, d.FullDateAlternateKey), d.CalendarYear
-ORDER BY d.CalendarYear, DATEPART(QUARTER, d.FullDateAlternateKey);
+WHERE d.CalendarYear = (
+    SELECT MAX(CalendarYear)
+    FROM DimDate
+)
+GROUP BY r.ResellerName, t.SalesTerritoryRegion
+ORDER BY TotalSales DESC;
