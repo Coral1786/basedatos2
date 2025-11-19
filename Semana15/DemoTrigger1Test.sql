dc02592481b3bb734ea28f559342a45088756d9b
Coral1786 <u21211786@utp.edu.pe>
2025-11-18 21:58:28 -0500

    Add test data insertion for SalesLog

---
+++ Semana15/DemoTrigger1Test.sql
@@ -0,0 +1,4 @@
+INSERT INTO dbo.SalesLog (SalesAmount, OrderDate)
+VALUES (2500.75, '2024-11-15');
+------------------------------------------------------------------------------------
+SELECT * FROM dbo.SalesLogAudit;
